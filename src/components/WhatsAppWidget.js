fbbdaf6a36d10d951f6e7b809f2c0d7c526ad604
harcourt hamsa <harcourthamsa@gmail.com>
2022-12-21 07:18:11 +0100

    change whatsapp number

--- src/components/WhatsAppWidget.js
+++ src/components/WhatsAppWidget.js
@@ -4,7 +4,7 @@ import WhatsAppLogo from "../assets/images/whatsapp-logo-png-2261.png";
 export default function WhatsAppWidget() {
   return (
     <a
-      href="https://api.whatsapp.com/send?phone=18147802502"
+      href="https://api.whatsapp.com/send?phone=180232024915"
       className="widget"
       style={{ zIndex: 3 }}
     >
